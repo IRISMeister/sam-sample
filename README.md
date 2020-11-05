36ccb0aee3e14df409846daaae3c54a4eae14fba
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2020-11-05 18:14:20 +0900

    doc

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # sam-sample
 <PRE>
 $ ./start.sh
-$ ./stop.sh
 
-
-	http://<ip-address-of-host-where-SAM-runs>:8080/api/sam/app/index.csp
+http://<ip-address-of-host-where-SAM-runs>:8080/api/sam/app/index.csp
 
 Login by SuperUser/SYS .
 Add new cluster.
@@ -12,4 +10,7 @@ Add new instance.
 ip : iris1
 port: 52773
 instance name: iris
+
+$ ./stop.sh
+
 </PRE>
--- sam-1.0.0.115-unix/stop.sh
+++ sam-1.0.0.115-unix/stop.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# removing voluems (durable sys) as well.
 docker-compose -p sam down -v
 
 #Without this, cluster registration will fail on second run 
 sudo chmod 777 config/prometheus/isc_*.yml
+
+#revert them to initial (because I'm removing durable sys)
 git checkout -- config/prometheus/isc_alert_rules.yml
 git checkout -- config/prometheus/isc_prometheus.yml
